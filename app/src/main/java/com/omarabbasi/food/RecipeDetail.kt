65b7f2c1c258578605fd7c2f8cea5b7341cb1664
Omar Abbasi <omarehsaan2@gmail.com>
2020-06-06 23:00:56 -0400

    add recipe detail

---
+++ app/src/main/java/com/omarabbasi/food/RecipeDetail.kt
@@ -0,0 +1,12 @@
+package com.omarabbasi.food
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecipeDetail : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.recipe_detail)
+    }
+}
